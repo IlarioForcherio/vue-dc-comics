e9dbaf5cc459119dba2f87d14bce948c48dc0385
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-23 16:36:13 +0100

    scheletro

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="" src="">
-   
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HeaderComp id="header" />
+    <JumbotronComp id="JumbotronComp"  />
+    <MainComp id="main" />
+    <FooterComp id="footer"  />
   </div>
 </template>
 
 <script>
-
+import HeaderComp from './components/HeaderComp.vue'
+import JumbotronComp from './components/JumbotronComp.vue'
+import MainComp from './components/MainComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
-   
+    HeaderComp,
+    JumbotronComp,
+    MainComp,
+    FooterComp
   }
 }
 </script>
 
+
+
 <style lang="scss">
+a{
+  color: black;
+  text-decoration: none;
+}
+
+li{
+  list-style: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
+
+
 </style>
